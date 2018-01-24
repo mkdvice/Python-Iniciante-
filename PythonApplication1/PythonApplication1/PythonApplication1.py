8f8b563cbeadcad1d73d02f9f121195ab43680bf
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-01-24 21:34:13 -0200

    Teste1

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome')
+print('Olá' , nome , '! Prazer em te conhecer!')
+
+dia = input('Qual é dia de seu nascimento  ?')
+mes = input('QUal é o mês de seu nascimento ?')
+ano = input('Qual é o ano de seu nascimento ?')
+print('Então, ' , nome , ' você nasceu no dia ', dia , 'de' , mes , 'de' , ano ,)
+
+
