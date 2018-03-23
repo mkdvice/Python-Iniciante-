a077675afb96272c161af3021edcb285886ea471
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-03-22 23:37:49 -0300

    Update Equação2grau.py

--- Equação2grau.py
+++ Equação2grau.py
@@ -1,6 +1,7 @@
 __author__ = '@MkDVice'
 
 import math
+import os
 
 # programa para calcular equação do segundo grau
 
@@ -11,16 +12,21 @@ c = int(input('Digite o valor de C: '))   # Entrada do valor de C
 delta = int(math.pow(b, 2)) - (4 * a * c)          # Cálculo do valor do delta
 
 print('''\n{}x² {}x {} = 0  \n'''.format(a, b, c)) # imprime a equação
-print('Delta = b²-4*a*c')                          # imprime a fórmula de delta
-print('Delta = {}² - 4*{}*{}'.format(b, a, c))     # imprime a resolução do delta
-print('Delta = {} \n'.format(delta))               # imprime o valor de delta
+print('Δ = b²-4*a*c')                          # imprime a fórmula de delta
+print('Δ = {}² - 4*{}*{}'.format(b, a, c))     # imprime a resolução do delta
+print('Δ = {} \n'.format(delta))               # imprime o valor de delta
 
 if delta < 0:   # Condição para caso o denta de negativo
     print('Vázio, número inexistente no conjunto dos reais') # resultado se delta negativo
+    os.system("pause") # Para o programa
 
 
 x1 = float(-b + (math.sqrt(delta))) // 2 * a    # Cálculo de X linha
 x2 = float(-b - (math.sqrt(delta))) // 2 * a    # Cálculo de X duas linhas
 
-print('O valor de X linha é {}'.format(x1))     # imprime o valor de X linha
+print('''x = (-b +- √∆) / (2 * a) \n''')        # imprime a fórmula de bhaskara
+print('x = [-({}) + √{}] / (2 * a)'.format(b, delta, a))  # imprime a fórmula de bhaskara com valores para x linha
+print('O valor de X linha é {}\n'.format(x1))   # imprime o valor de X linha
+
+print('x = [-({}) - √{}] / (2 * a)'.format(b, delta, a))  # imprime a fórmula de bhaskara com valores para x duas linhas
 print('O valor de x duas linhas {}'.format(x2)) # imprime o valor de X duas linhas
