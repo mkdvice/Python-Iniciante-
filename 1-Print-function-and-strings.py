fe723654aadc6144e1c3d922812c5c02d99c2003
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2019-05-09 10:18:56 -0300

    prints

---
+++ 1-Print-function-and-strings.py
@@ -0,0 +1,7 @@
+print('This is an example of print function') # Simples print
+
+print('we\'re going to the store') # Uso da barra invertida para colocar uma aspa simples
+
+print('Hi','there') # Concatenando print
+
+print(float('8.5')+5) # convertendo string para float
